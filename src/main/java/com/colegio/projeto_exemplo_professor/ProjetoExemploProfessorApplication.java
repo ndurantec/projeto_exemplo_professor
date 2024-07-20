076040d6c1557041c59d01243d1d46aa57ea6549
ndurantec <norberto.duran@escola.pr.gov.br>
2024-07-20 01:02:41 -0300

    Arquivos iniciais do projeto

---
+++ src/main/java/com/colegio/projeto_exemplo_professor/ProjetoExemploProfessorApplication.java
@@ -0,0 +1,15 @@
+package com.colegio.projeto_exemplo_professor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+//@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class ProjetoExemploProfessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoExemploProfessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/colegio/projeto_exemplo_professor/ProjetoExemploProfessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colegio.projeto_exemplo_professor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoExemploProfessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
